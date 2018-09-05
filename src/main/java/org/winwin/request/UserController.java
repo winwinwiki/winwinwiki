2ec4eb5e4a6c31c8a163c7491f74b08592479fb0
ssriraman <ssriraman@users.noreply.github.com>
2018-09-04 22:44:43 -0700

    created user profile (#6)
    * changes for user
    
    * changes for user

--- src/main/java/org/winwin/request/UserController.java
+++ src/main/java/org/winwin/request/UserController.java
@@ -8,9 +8,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.winwin.model.ApplicationUser;
 import org.winwin.repository.ApplicationUserRepository;
@@ -29,29 +31,53 @@ public class UserController {
 
 	@Autowired
 	private ApplicationUserRepository auRepository;
+	
+	
+	@GetMapping("/user/{id}")
+	public ApplicationUser getUser(@Valid @PathVariable("id") long id) {
+		Optional<ApplicationUser> result = auRepository.findById(id);
+		return removeCriticalFields(result.orElse(null));
+	}
+	
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+	public String deleteUser(@Valid @PathVariable("id") long id) {
+		auRepository.deleteById(id);
+		return SUCCESS;
+	}
 
-	@PostMapping("/searchUser")
-	public ApplicationUser getUser(@Valid @RequestBody ApplicationUser user) {
-		if (user.getId() != null) {
-			Optional<ApplicationUser> result = auRepository.findById(user.getId());
-			result.orElse(null);
-		}
-		if (user.getUsername() != null) {
-			return auRepository.findByUsername(user.getUsername());
+	@PostMapping("/user")
+	public ApplicationUser createUser(@Valid @RequestBody ApplicationUser user) {
+		ApplicationUser appUser = auRepository.findByUsername(user.getUsername());
+		if (appUser == null) {
+			user.setPassword(passwdEncoder.encode(user.getPassword()));
+			user.setCreatedAt(new Date());
+			user.setUpdatedAt(new Date());
+			appUser = auRepository.save(user);
+			return removeCriticalFields(appUser);
 		}
 		return null;
 	}
 
-	@PutMapping("/createUser")
-	public String createUser(@Valid @RequestBody ApplicationUser user) {
-		user.setPassword(passwdEncoder.encode(user.getPassword()));
-		user.setCreatedAt(new Date());
-		user.setUpdatedAt(new Date());
-		auRepository.save(user);
-		return SUCCESS;
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
+	public String updateUser(@Valid @PathVariable("id") long id, @Valid @RequestBody ApplicationUser user) {
+		Optional<ApplicationUser> result = auRepository.findById(id);
+		if( result.isPresent() ) {
+			ApplicationUser userInDB = result.get();
+			userInDB.setUpdatedAt(new Date());
+			userInDB.setLocation(user.getLocation());
+			userInDB.setRole(user.getRole());
+			userInDB.setFirstname(user.getFirstname());
+			userInDB.setLastname(user.getLastname());
+			userInDB.setEmail(user.getEmail());
+			userInDB.setPassword(passwdEncoder.encode(user.getPassword()));
+			auRepository.save(userInDB);
+			return SUCCESS;
+		}else {
+			return FAILURE;
+		}
 	}
 
-	@PostMapping("/updatePassword")
+	@PostMapping("/password/update")
 	public String updatePassword(@Valid @RequestBody ApplicationUser user) {
 		ApplicationUser appUser = auRepository.findByUsername(user.getUsername());
 		if (appUser != null) {
@@ -60,9 +86,16 @@ public class UserController {
 			auRepository.save(user);
 			return SUCCESS;
 		} else {
-			log.error("User not found :" + user.getUsername());
+			log.error("User not found :" + user.getEmail());
 			return FAILURE;
 		}
+	}
+	
+	private ApplicationUser removeCriticalFields(ApplicationUser user) {
+		if( user != null ) {
+			user.setPassword("*****");
+		}
+		return user;
 	}
 
 }
