5f0e1059957c2f2c3b179c83401565e46f2abcb4
Ramprasad Gopalakrishnan <ramprasadg@uber.com>
2018-09-05 23:19:53 -0700

    adding unit test for ping controller

---
+++ src/test/java/org/winwin/request/PingControllerTest.java
@@ -0,0 +1,31 @@
+package org.winwin.request;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.winwin.restservice.RestServiceApplication;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = RestServiceApplication.class)
+public class PingControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/ping",
+                String.class)).contains("OK");
+    }
+}
